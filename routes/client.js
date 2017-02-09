f06fe94c316fbe6a5b64e407c592f335fe5f5160
markgreenburg <marek.greenburg@gmail.com>
2017-02-09 10:43:41 -0600

    Added client-side routes

---
+++ routes/client.js
@@ -0,0 +1,22 @@
+/**
+ * Router for clientside endpoints. Mounted at /
+ */
+'use strict';
+
+const router = require('express').Router();
+const user = require('../models/user');
+const product = require('../models/product');
+const order = require('../models/order');
+
+router.post('/user/signup', user.createUser);
+router.post('/user/update', user.auth, user.updateUser);
+router.get('/user/login', (req, res) => {
+    res.render('../templates/login');
+});
+
+router.post('/cart', user.auth, product.addToCart);
+
+router.route('/cart/checkout')
+    .get(user.auth, order.getCart)
+    .post(user.auth, order.createOrder);
+module.exports = router;
